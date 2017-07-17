9497017a6f669a3be02624503543c9c3f373a9aa
Kia Fathi <kiafathi@me.com>
2017-07-17 14:20:01 -0700

    (feat) Very very basic project scaffold

---
+++ styleguide.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  sections: [
+    {
+      name: 'Introduction',
+      content: './src/docs/Introduction.md',
+    },
+    {
+      name: 'UI Components',
+      components: './src/components/*.js',
+    },
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-module.exports = options => ({
+module.exports = () => ({
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'mira-elements.js',
     libraryTarget: 'umd',
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
   },
   module: {
     rules: [
