1d3ffe20f4b7627676154201757a3d39bcbbc665
Leon Li <leon.li.1025@gmail.com>
2019-02-07 16:49:26 -0500

    Add checkered background to ThemeWrapper (#155)

--- src/styleguide/components/ThemeWrapper/ThemeWrapper.styles.ts
+++ src/styleguide/components/ThemeWrapper/ThemeWrapper.styles.ts
@@ -7,6 +7,14 @@ const styles = createStyles({
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
+    backgroundImage: [
+      'linear-gradient(45deg, rgba(128,128,128,0.05) 25%, transparent 25%)',
+      'linear-gradient(135deg, rgba(128,128,128,0.05) 25%, transparent 25%)',
+      'linear-gradient(45deg, transparent 75%, rgba(128,128,128,0.05) 75%)',
+      'linear-gradient(135deg, transparent 75%, rgba(128,128,128,0.05) 75%)',
+    ].join(','),
+    backgroundSize: '1em 1em', // must be square
+    backgroundPosition: '0 0, 0.5em 0, 0.5em -0.5em, 0px 0.5em', // must be half of background-size
   },
   actions: {
     position: 'absolute',
