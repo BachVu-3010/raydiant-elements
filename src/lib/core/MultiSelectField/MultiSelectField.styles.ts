f503c9498efc1e15ba7eba6dfb34c1e4d787b217
Jordan <github@jschr.io>
2020-08-28 11:20:57 -0400

    Fix MultiSelectField label styles

--- src/lib/core/MultiSelectField/MultiSelectField.styles.ts
+++ src/lib/core/MultiSelectField/MultiSelectField.styles.ts
@@ -18,16 +18,8 @@ const styles = (theme: Theme) =>
       boxShadow: 'inset 0px 1px 5px rgba(0, 0, 0, 0.2)',
       ...scrollable(),
     },
-    label: {
-      fontSize: theme.fontSizes.sm,
-      color: theme.palette.text.secondary,
-      marginBottom: theme.spacing(0.5),
-    },
     disabled: {
       overflow: 'hidden',
-    },
-    error: {
-      color: theme.palette.error.main,
     },
   });
 
