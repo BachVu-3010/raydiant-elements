261f6c3a8c5301ba694051ec104ac39df1eb5724
Jordan <github@jschr.io>
2021-02-10 18:14:27 -0500

    Remove unnesssary line height

--- src/lib/core/Select/Select.styles.ts
+++ src/lib/core/Select/Select.styles.ts
@@ -20,7 +20,6 @@ export default makeStyles((theme: Theme) =>
     select: {
       width: '100%',
       height: '100%',
-      lineHeight: '40px',
       boxSizing: 'border-box',
       padding: theme.spacing(0, 2),
       display: 'flex',
