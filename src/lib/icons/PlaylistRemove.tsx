098cb6014bbd7a66f7fbe783ec5d10d949d8e5ca
Jordan <github@jschr.io>
2021-07-15 09:28:41 -0400

    Update playlist remove icon

--- src/lib/icons/PlaylistRemove.tsx
+++ src/lib/icons/PlaylistRemove.tsx
@@ -7,7 +7,7 @@ export const PlaylistRemove = (props: PlaylistRemoveProps) => (
   <SvgIcon {...props} viewBox="0 0 24 24">
     <path
       fill="currentColor"
-      d="M2,16H10V14H2M12,14V16H22V14M14,6H2V8H14M14,10H2V12H14V10Z"
+      d="M20 17v2H8v-2h12zm0-4v2H8v-2h12zM6 2v3c4.284 0 7.87 2.994 8.778 7.003h-.869c-.466 0-.892-.002-1.2-.005C11.844 9.11 9.163 7 6 7v3L2 6l4-4zm14 7v2h-4V9h4z"
     />
   </SvgIcon>
 );
